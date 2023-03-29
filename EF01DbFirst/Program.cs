622a8ce24694bcb0d13e3002b683eebf32c6d0f4
ciminoce <ciminoce@yahoo.com.ar>
2023-03-29 11:17:48 -0300

    Agregar archivos de proyecto.

---
+++ EF01DbFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF01DbFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context=new EF01Curso2023Entities())
+            {
+                context.Database.Log = Console.WriteLine;
+                var listaCursos = context.Cursos.ToList();
+                foreach (var curso in listaCursos)
+                {
+                    Console.WriteLine($"{curso.NombreCurso} {curso.Descripcion} {curso.Vacantes}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
